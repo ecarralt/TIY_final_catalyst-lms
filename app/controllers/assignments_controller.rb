7375b35e8575b77883a8b3f46ec375db49782310
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-27 12:31:34 -0500

    Teachers can render all assignments order by assignment_number

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,62 @@
+class AssignmentsController < ApplicationController
+
+  def index
+    if @current_user.usertype == "student"
+      @assignments = Assignment.all.where(released: "1").order(assignment_number: :asc)
+    else
+      @assignments = Assignment.all.order(assignment_number: :asc)
+    end
+  end
+
+  def show
+    @assignment = Assignment.find_by id: params[:id]
+  end
+
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @assignment = Assignment.new
+    @assignment.assignment_number = params[:assignment][:assignment_number].to_i
+    @assignment.title = params[:assignment][:title]
+    @assignment.content = params[:assignment][:content]
+    @assignment.author = @current_user.full_name
+    @assignment.released = params[:assignment][:released]
+    @assignment.user_id = @current_user.id
+    if @assignment.save
+
+      redirect_to assignments_path
+    else
+      render :new, notice: "Assignment created"
+    end
+  end
+
+  def edit
+    @assignment = Assignment.find_by id: params[:id]
+  end
+
+  def update
+    @assignment = Assignment.find_by id: params[:id]
+    @assignment.assignment_number = params[:assignment][:assignment_number].to_i
+    @assignment.title = params[:assignment][:title]
+    @assignment.content = params[:assignment][:content]
+    @assignment.author = @current_user.full_name
+    @assignment.released = params[:assignment][:released]
+    @assignment.user_id = @current_user.id
+    if @assignment.save
+      redirect_to assignments_path, notice: "Assignment updated"
+    else
+      flash[:notice] = "Something went wrong saving your update, please make sure all contents are included and try again."
+      render :edit
+    end
+
+  end
+
+  def delete
+    @assignment = Lesson.find_by id: params[:id]
+    @assignment.destroy
+    redirect_to assignments_path, notice: "Assignment deleted successfully"
+  end
+
+end

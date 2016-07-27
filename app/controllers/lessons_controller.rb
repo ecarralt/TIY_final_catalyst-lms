6dac420607459066db5b2c9bdaeee018e7093f0b
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-27 14:48:35 -0500

    different app layouts created for welcome, students (application), and teachers

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,11 @@
 class LessonsController < ApplicationController
-    # layout "teachers"
+
+  layout :resolve_layout
 
   def dashboard
     if @current_user.usertype == "student"
       @lessons = Lesson.all.where(released: "1").order(lesson_number: :asc)
     else
-      layout
       @lessons = Lesson.all.order(lesson_number: :asc)
     end
   end
@@ -59,6 +59,16 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find_by id: params[:id]
     @lesson.destroy
     redirect_to lessons_path, notice: "Lesson deleted successfully"
+  end
+
+  def resolve_layout
+    case @current_user.usertype
+    when "student"
+      "application"
+    when "teacher"
+      "teachers"
+    else
+    end
   end
 
 end

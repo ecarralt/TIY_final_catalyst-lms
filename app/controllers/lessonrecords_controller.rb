30b8b85544f84c2676a804c021c9ed5279896ff7
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-31 15:42:30 -0500

    Error fixed, students and teachers can now navigate lessons and assignments consistently.

--- app/controllers/lessonrecords_controller.rb
+++ app/controllers/lessonrecords_controller.rb
@@ -3,7 +3,8 @@ class LessonrecordsController < ApplicationController
   layout :resolve_layout
 
   def create
-    if Lessonrecord.find_by(user_id: params[:u_id], lesson_id: params[:l_id] == nil)
+    #case 1: complete lesson record does not exit for this lesson/user
+    if Lessonrecord.find_by(user_id: params[:u_id], lesson_id: params[:l_id]) == nil
       @lrecord = Lessonrecord.new
       @lrecord.complete = "yes"
       @lrecord.lesson_id = params[:l_id]
@@ -13,7 +14,16 @@ class LessonrecordsController < ApplicationController
       else
         redirect_to lesson_path(id: params[:l_id]), notice: "Something went wrong setting your lesson as complete, please try again."
       end
-    else
+    #case 2: complete lesson record exists for this lesson/user, and the student has not completed the lesson
+    elsif @lrecord = Lessonrecord.find_by(user_id: params[:u_id], lesson_id: params[:l_id], complete: "no")
+      @lrecord.complete ="yes"
+      if @lrecord.save
+        redirection_to_next_lesson
+      else
+        redirect_to lesson_path(id: params[:l_id]), notice: "Something went wrong setting your lesson as complete, please try again."
+      end
+    #case 3: complete lesson record exists for this lesson/user, and the student has already completed the lesson  
+    elsif @lrecord = Lessonrecord.find_by(user_id: params[:u_id], lesson_id: params[:l_id], complete: "yes")
       redirection_to_next_lesson
     end
   end

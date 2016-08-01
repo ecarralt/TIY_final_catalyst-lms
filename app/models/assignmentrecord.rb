ce508d39caf9628d37c14833b2b2e7d47992cf00
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-31 22:39:48 -0500

    have not resolved validation issue yet

--- app/models/assignmentrecord.rb
+++ app/models/assignmentrecord.rb
@@ -1,14 +1,22 @@
 class Assignmentrecord < ActiveRecord::Base
-
-  validates :comments, presence: true, if: "contentfile.nil?", if: "url_link.nil?"
+  # validates :comments, presence: true, if: "contentfile.nil?", if: "url_link.nil?"
 
   attachment :contentfile, extension: ["pdf", "doc", "docx"]
+  validate :at_least_one_submission_record
 
   belongs_to :user
   belongs_to :assignment
 
   def file_extension
     self.content_type.split("/").last.to_sym
+  end
+
+  def at_least_one_submission_record
+    if (self.comments.present? == false && self.url_link.present? == false)
+      return false
+    else
+      return true
+    end
   end
 
 end

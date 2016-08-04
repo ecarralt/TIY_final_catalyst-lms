ca58a53a40bdbb4d99e5569f799de060fc17a645
Enrique Carral Trevino <ecarralt@gmail.com>
2016-08-04 11:53:53 -0500

    Added student scorecard on students/show view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,9 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @assignments = Assignment.all.where(released: "1").order(assignment_number: :asc)
+    @lessons = Lesson.all.where(released: "1").order(lesson_number: :asc)
+
     @student = User.find_by(id: params[:id])
     @current_assignments = Assignment.all.where(released: "1").order(assignment_number: :asc)
     @current_lessons = Lesson.all.where(released:"1").order(lesson_number: :asc)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
   extensions = {
-    autolink: true,
-    no_intra_emphasis: true,
-    fenced_code_blocks: true
+    autolink: true, ##parse links even when they are not enclosed in <> characters. Autolinks for the http, https and ftp protocols will be automatically detected. Email addresses and http links without protocol, but starting with www are also handled.
+    no_intra_emphasis: true, ##do not parse emphasis inside of words. Strings such as foo_bar_baz will not generate <em> tags.
+    fenced_code_blocks: true ##parse fenced code blocks, PHP-Markdown style. Blocks delimited with 3 or more ~ or backticks will be considered as code, without the need to be indented. An optional language name may be added at the end of the opening fence for the code block.
   }
 
   render_options = {
-    filter_html: true,
-    hard_wrap: true
+    filter_html: true, ##do not allow any user-inputted HTML in the output.
+    hard_wrap: true ##insert HTML <br> tags inside paragraphs where the original Markdown document had newlines (by default, Markdown ignores these newlines).
   }
 
 

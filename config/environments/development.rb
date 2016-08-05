2858b03cc828ded4b096f4df6339d935df549931
Enrique Carral Trevino <ecarralt@gmail.com>
2016-08-05 15:48:11 -0500

    Added gem 'wkhtmltopdf-heroku' for pdf on heroku to work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
     # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-    config.assets.precompile += ['blueprint/screen.css', 'pdf.css', 'jquery.ui.datepicker.js', 'pdf.js', ...etc...]
+    # config.assets.precompile += ['blueprint/screen.css', 'pdf.css', 'jquery.ui.datepicker.js', 'pdf.js', ...etc...]
 
     config.action_mailer.smtp_settings = {
       address:              'smtp.sendgrid.net',

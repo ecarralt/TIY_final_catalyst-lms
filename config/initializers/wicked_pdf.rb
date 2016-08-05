6c3dd8a543e3ec6fa22227ed640762246f974b93
Enrique Carral Trevino <ecarralt@gmail.com>
2016-08-05 16:13:14 -0500

    Changed path on wicked_pdf initializer for heroku: testing

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,12 +9,14 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
-    exe_path: '/usr/local/bin/wkhtmltopdf'
+  # exe_path: '/usr/local/bin/wkhtmltopdf'
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+
+  exe_path: Gem.bin_path('wkhtmltopdf-heroku')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)

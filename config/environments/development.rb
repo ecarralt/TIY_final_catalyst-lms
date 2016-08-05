d4485e4f637fb93c4f827c40396e194bc9afd30b
Enrique Carral Trevino <ecarralt@gmail.com>
2016-08-05 13:05:04 -0500

    Changed smtp settings and host to sendgrid, trying on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,17 +50,27 @@ Rails.application.configure do
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  #
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'gmail.com',
+  #   user_name:            ENV["GMAIL_USERNAME"],
+  #   password:             ENV["GMAIL_PASSWORD"],
+  #   authentication:       "plain",
+  #   enable_starttls_auto: true  }
 
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       "plain",
-    enable_starttls_auto: true  }
+    # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 587,
+      domain:               'heroku.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       "plain",
+      enable_starttls_auto: true  }
 
+    config.action_mailer.default_url_options = { host: 'tiy-finalproject-catalyst.herokuapp.com' }
 
 end

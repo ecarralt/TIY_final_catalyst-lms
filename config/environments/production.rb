c68934db8419a929639f4cb8d3163df450dad3a3
Enrique Carral Trevino <ecarralt@gmail.com>
2016-08-05 13:12:56 -0500

    Modified production file to send email from heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   #
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
@@ -92,4 +91,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true  }
+
+  config.action_mailer.default_url_options = { host: 'tiy-finalproject-catalyst.herokuapp.com' }
+
+
+
+
+
+
+
 end
